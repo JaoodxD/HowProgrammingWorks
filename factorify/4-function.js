d6e183e73876ca279d109c6658e09b82b958c079
JaoodxD <jaood96@gmail.com>
2022-08-25 15:53:10 +0300

    example of function factory

---
+++ factorify/4-function.js
@@ -0,0 +1,20 @@
+const logger = (level = 'info') => {
+    const colors = {
+        warning: '\x1b[1;33m',
+        error: '\x1b[0;31m',
+        info: '\x1b[1;37m'
+    };
+
+    const color = colors[level];
+
+    return s => {
+        const date = new Date().toISOString();
+        console.log(color,date,'\t',s);
+    }
+}
+
+const warning = logger('warning');
+const info = logger();
+
+warning('hello');
+info('world');
