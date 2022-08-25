6e906b601632181a544c6c75c95491f4be79843a
JaoodxD <jaood96@gmail.com>
2022-08-25 16:14:00 +0300

    added jsdoc to colors enum

--- factorify/4-function.js
+++ factorify/4-function.js
@@ -1,10 +1,19 @@
-const logger = (level = 'info') => {
-    const colors = {
-        warning: '\x1b[1;33m',
-        error: '\x1b[0;31m',
-        info: '\x1b[1;37m'
-    };
+/**
+ * @enum {'warning'| 'error' | 'info'}
+ */
+const colors = {
+    warning: '\x1b[1;33m',
+    error: '\x1b[0;31m',
+    info: '\x1b[1;37m'
+};
 
+/**
+ * 
+ * @param {colors} level 
+ * @returns 
+ */
+const logger = (level = 'info') => {
+    
     const color = colors[level];
 
     return s => {
@@ -15,6 +24,8 @@ const logger = (level = 'info') => {
 
 const warning = logger('warning');
 const info = logger();
+const error = logger('error');
 
 warning('hello');
 info('world');
+error('!');
