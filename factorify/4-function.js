fd0ce88a46f511ac4cb6f5566f2954ed9ff9bc60
JaoodxD <jaood96@gmail.com>
2022-08-25 16:30:47 +0300

    Prettify Code

--- factorify/4-function.js
+++ factorify/4-function.js
@@ -1,20 +1,16 @@
 //@ts-check
-
 const colors = {
     'warning': '\x1b[1;33m',
     'error': '\x1b[0;31m',
     'info': '\x1b[1;37m'
 };
 
-/**
- * 
+/** 
  * @param {'warning' | 'error' | 'info'} level 
  * @returns 
  */
 const logger = (level = 'info') => {
-
     const color = colors[level];
-
     return s => {
         const date = new Date().toISOString();
         console.log(color, date, '\t', s);
